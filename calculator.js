3c5716dd540c1d65bee51e4302024a7ab32a276b
Tobi <tobiedeh@gmail.com>
2022-12-01 05:22:18 -0600

    Calculator is completely functional. Adds, subtracts, multiplies and divides whole numbers and decimals. Returns error when dividing by 0. Everything works.

--- calculator.js
+++ calculator.js
@@ -10,7 +10,11 @@ const mi = document.getElementById('mi')
 const di = document.getElementById('di')
 const mu = document.getElementById('mu')
 const equal = document.getElementById('eq')
+const del = document.getElementById('del')
+const reset = document.getElementById('res')
 let fail = false
+let equ = false
+let nin = false
 let firstnum = 0
 let lastnum = 0
 
@@ -20,11 +24,21 @@ function answer(firstnum, lastnum) {
         di.style.cssText = ''
         mu.style.cssText = ''
         pl.style.cssText = ''
-        plus.checked = false
+        plus.checked = true
         divide.checked = false
         minus.checked = false
         multi.checked = false
-        return firstnum + lastnum
+        let num = firstnum + lastnum
+        let numstr = String(num)
+        let declength = 0
+        if (numstr.includes('.')) {
+            declength = numstr.split('.')[1].length
+        }
+        if (declength >= 5) {
+            return num.toFixed(5)
+        } else {
+            return (firstnum + lastnum)
+        }
     } 
     else if (minus.checked) {
         mi.style.cssText = ''
@@ -33,9 +47,19 @@ function answer(firstnum, lastnum) {
         pl.style.cssText = ''
         plus.checked = false
         divide.checked = false
-        minus.checked = false
+        minus.checked = true
         multi.checked = false
-        return firstnum - lastnum
+        let num = firstnum - lastnum
+        let numstr = String(num)
+        let declength = 0
+        if (numstr.includes('.')) {
+            declength = numstr.split('.')[1].length
+        }
+        if (declength >= 5) {
+            return num.toFixed(5)
+        } else {
+            return (firstnum - lastnum)
+        }
     }
     else if (divide.checked) {
         mi.style.cssText = ''
@@ -43,10 +67,24 @@ function answer(firstnum, lastnum) {
         mu.style.cssText = ''
         pl.style.cssText = ''
         plus.checked = false
-        divide.checked = false
+        divide.checked = true
         minus.checked = false
         multi.checked = false
-        return firstnum / lastnum
+        if (lastnum == 0) {
+            alert("Hey! You Can't Do That!")
+            return 0
+        }
+        let num = firstnum / lastnum
+        let numstr = String(num)
+        let declength = 0
+        if (numstr.includes('.')) {
+            declength = numstr.split('.')[1].length
+        }
+        if (declength >= 5) {
+            return num.toFixed(5)
+        } else {
+            return (firstnum / lastnum)
+        }
     }
     else if (multi.checked) {
         mi.style.cssText = ''
@@ -56,8 +94,18 @@ function answer(firstnum, lastnum) {
         plus.checked = false
         divide.checked = false
         minus.checked = false
-        multi.checked = false
-        return firstnum * lastnum
+        multi.checked = true
+        let num = firstnum * lastnum
+        let numstr = String(num)
+        let declength = 0
+        if (numstr.includes('.')) {
+            declength = numstr.split('.')[1].length
+        }
+        if (declength >= 5) {
+            return num.toFixed(5)
+        } else {
+            return (firstnum * lastnum)
+        }
     }
 }
 
@@ -67,14 +115,19 @@ for (let i = 0; i < btn.length; i++) {
     });
     btn[i].addEventListener('mouseup', function(e){
         btn[i].style.cssText = 'box-shadow: 2px 5px gray;'
-        if (btn[i].textContent == '/' || btn[i].textContent == 'x' 
+        if ((plus.checked || minus.checked || divide.checked || multi.checked || nin == true)
+        && nin == true) {
+            nin = false
+            input.textContent = btn[i].textContent
+        }
+        else if (btn[i].textContent == '/' || btn[i].textContent == 'x' 
             || btn[i].textContent == "-" || btn[i].textContent == '+' ||
-            btn[i].textContent == '=' || input.textContent.length == 9) { 
+            btn[i].textContent == '=' || input.textContent.length == 9 ||
+            btn[i].textContent == 'DEL') { 
             input.textContent = input.textContent 
             fail = false
         }
-        else if (btn[i].textContent == '.' && input.textContent.includes('.')
-                && !input.textContent.includes('0')){
+        else if (btn[i].textContent == '.' && input.textContent.includes('.')){
             input.textContent = input.textContent
         }
         else if (btn[i].textContent == '.' && input.textContent == '0') {
@@ -83,10 +136,16 @@ for (let i = 0; i < btn.length; i++) {
         else if (input.textContent == '0') {
             input.textContent = btn[i].textContent
         }
-        else if ((plus.checked || minus.checked || divide.checked || multi.checked)
-                  && fail == false) {
-            input.textContent = btn[i].textContent
+        else if ((plus.checked || minus.checked || divide.checked || multi.checked || nin == true)
+                  && fail == false || equ == true) {
+            if (btn[i].textContent == '.') {
+                input.textContent = '0' + btn[i].textContent
+            } else {
+                input.textContent = btn[i].textContent
+            }
             fail = true
+            equ = false
+            nin = false
         }
         else {
             input.textContent = input.textContent + btn[i].textContent
@@ -95,6 +154,21 @@ for (let i = 0; i < btn.length; i++) {
 }
 
 pl.addEventListener('click', function(){
+    if (minus.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    else if (divide.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    else if (multi.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    if (input.textContent.length == 9) {
+        nin = true
+    }
     mi.style.cssText = ''
     di.style.cssText = ''
     mu.style.cssText = ''
@@ -104,11 +178,25 @@ pl.addEventListener('click', function(){
     minus.checked = false
     multi.checked = false
     firstnum = Number(input.textContent)
-    console.log(firstnum)
 
 })
 
 mi.addEventListener('click' , function(){
+    if (plus.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    else if (divide.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    else if (multi.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    if (input.textContent.length == 9) {
+        nin = true
+    }
     pl.style.cssText = ''
     di.style.cssText = ''
     mu.style.cssText = ''
@@ -118,10 +206,24 @@ mi.addEventListener('click' , function(){
     plus.checked = false
     multi.checked = false
     firstnum = Number(input.textContent)
-    console.log(firstnum)
 })
 
 di.addEventListener('click', function(){
+    if (minus.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    else if (plus.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    else if (multi.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    if (input.textContent.length == 9) {
+        nin = true
+    }
     mi.style.cssText = ''
     pl.style.cssText = ''
     mu.style.cssText = ''
@@ -131,10 +233,24 @@ di.addEventListener('click', function(){
     minus.checked = false
     multi.checked = false
     firstnum = Number(input.textContent)
-    console.log(firstnum)
 })
 
 mu.addEventListener('click', function(){
+    if (minus.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    else if (divide.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    else if (plus.checked) {
+        lastnum = Number(input.textContent)
+        input.textContent = answer(firstnum, lastnum)
+    }
+    if (input.textContent.length == 9) {
+        nin = true
+    }
     mi.style.cssText = ''
     di.style.cssText = ''
     pl.style.cssText = ''
@@ -144,11 +260,35 @@ mu.addEventListener('click', function(){
     minus.checked = false
     plus.checked = false
     firstnum = Number(input.textContent)
-    console.log(firstnum)
 })
 
 equal.addEventListener('click', function() {
     lastnum = Number(input.textContent)
-    console.log(lastnum)
     input.textContent = answer(firstnum, lastnum)
+    equ = true
+    multi.checked = false
+    divide.checked = false
+    minus.checked = false
+    plus.checked = false
+})
+
+res.addEventListener('click', function() {
+    mi.style.cssText = ''
+    di.style.cssText = ''
+    pl.style.cssText = ''
+    mu.style.cssText = ''
+    multi.checked = false
+    divide.checked = false
+    minus.checked = false
+    plus.checked = false
+    input.textContent = "0"
+})
+
+del.addEventListener('click', function() {
+    equ = false
+    let len = input.textContent.length
+    let newText = input.textContent.slice(0, len-1)
+    if (len == 1) {
+        input.textContent = '0'
+    } else (input.textContent = newText)
 })
